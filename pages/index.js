c1c7a93b8f90c014a2c6a73b23f4c698d67c85e8
ChengCalvin <calvincheng_95@hotmail.com>
2020-09-18 13:21:47 -0400

    bug check nova content test

--- pages/index.js
+++ pages/index.js
@@ -161,16 +161,17 @@ function Home({ t }) {
           <div className={styles.successstorycontent}>
             <div className={styles.successstorytitle}>
               {t("REL REALTY ADVISORS IS PROUD TO PRESENT ITS FIRST PROJECT")}
+              <>
+                {i18n.language === "fr" ? (
+                  <div className={styles.novaintroparagraph}>
+                    {t("NovaIntro")}
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </>
             </div>
-            <>
-              {i18n.language === "fr" ? (
-                <div className={styles.novaintroparagraph}>
-                  {t("NovaIntro")}
-                </div>
-              ) : (
-                <></>
-              )}
-            </>
+
             <img
               className={styles.novatitle}
               src="/images/novatitleimage.png"
